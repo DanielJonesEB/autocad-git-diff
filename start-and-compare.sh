62ea2041e0affed73d00b8ce8c4e8e9b52fffc37
Daniel Jones <daniel.jones@engineerbetter.com>
2022-06-23 15:15:08 +0100

    diff between current and given ref

--- start-and-compare.sh
+++ start-and-compare.sh
@@ -1,3 +1,18 @@
 #! /usr/bin/env bash
 
-"/Applications/Autodesk/AutoCAD 2023/AutoCAD 2023.app/Contents/MacOS/AutoCAD" -nologo -b /Users/danieljones/workspace/autocad-compare/compare.scr /Users/danieljones/workspace/autocad-compare/b.dwg
+set -euxo pipefail
+
+FILENAME=$1
+OTHER_REF=$2
+
+OTHER_PATH=$(mktemp).dwg
+echo "$OTHER_PATH"
+git show "$OTHER_REF:$FILENAME" > "$OTHER_PATH"
+
+COMPARE_SCRIPT_PATH=$(mktemp).scr
+echo "-COMPARE $OTHER_PATH" > "$COMPARE_SCRIPT_PATH"
+
+"/Applications/Autodesk/AutoCAD 2023/AutoCAD 2023.app/Contents/MacOS/AutoCAD" \
+  -nologo \
+  -b "$COMPARE_SCRIPT_PATH" \
+  "/Users/danieljones/workspace/autocad-compare/$FILENAME"
