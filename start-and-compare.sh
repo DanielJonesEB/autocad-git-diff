891f7101dd88bde921978906aa9f5fceab2fc501
Daniel Jones <daniel.jones@engineerbetter.com>
2022-06-23 12:55:11 +0100

    reverse ordering of hardcoded compare

--- start-and-compare.sh
+++ start-and-compare.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-"/Applications/Autodesk/AutoCAD 2023/AutoCAD 2023.app/Contents/MacOS/AutoCAD" -nologo -b /Users/danieljones/workspace/autocad-compare/compare.scr /Users/danieljones/workspace/autocad-compare/a.dwg
+"/Applications/Autodesk/AutoCAD 2023/AutoCAD 2023.app/Contents/MacOS/AutoCAD" -nologo -b /Users/danieljones/workspace/autocad-compare/compare.scr /Users/danieljones/workspace/autocad-compare/b.dwg
